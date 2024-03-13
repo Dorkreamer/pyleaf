8fce09bfa283a822b52761a3475daea0f0af1fea
Moonlight Dorkreamer <moonlightdorkreamer@gmail.com>
2024-03-13 10:42:25 -0300

    OS logos

---
+++ logos.py
@@ -0,0 +1,194 @@
+imgs = {
+    "bedrock": {
+        "lines": [
+            " __       ",
+            " \\ \\___   ",
+            "  \\  _ \\  ",
+            "   \\___/  ",
+            "          "
+        ],
+        "fore_color": "white",
+        "bg_color": "black",
+        "text_color":"white"
+    },
+    "endeavouros": {
+        "lines": [
+            "     __   ",
+            "    /  \\  ",
+            "  /     | ",
+            " '_____/  ",
+            "          "
+        ],
+        "fore_color": "pink",
+        "bg_color": "pink",
+        "text_color": "pink"
+    },
+    "arch": {
+        "lines": [
+            "          ",
+            "    /\\    ",
+            "   /  \\   ",
+            "  /_/\\_\\  ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "cyan",
+        "text_color":"cyan"
+    },
+    "gentoo": {
+        "lines": [
+            "   ___    ",
+            "  [  _ \\  ",
+            "   >   /  ",
+            "  (__ /   ",
+            "          "
+        ],
+        "fore_color": "white",
+        "bg_color": "white",
+        "text_color": "white"
+    },
+    "debian": {
+        "lines": [
+            "     __   ",
+            "  '/  .\\' ",
+            "  |  (_'  ",
+            "   \\      ",
+            "          "
+        ],
+        "fore_color": "red",
+        "bg_color": "red",
+        "text_color": "red"
+    },
+    "void": {
+        "lines": [
+            "    __    ",
+            "  ,\\   \\  ",
+            " |'\\()\\,| ",
+            "  \\ __\\'  ",
+            "          "
+        ],
+        "fore_color": "green",
+        "bg_color": "black",
+        "text_color": "green"
+    },
+    "fedora": {
+        "lines": [
+            "   ____   ",
+            "  /  _ \\  ",
+            " | C/-  | ",
+            " |_____/  ",
+            "          "
+        ],
+        "fore_color": "blue",
+        "bg_color": "black",
+        "text_color": "blue"
+    },
+    "alpine": {
+        "lines": [
+            "          ",
+            "   /\\     ",
+            "  //\\\\/  ",
+            " //  \\\\ ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "blue",
+        "text_color": "blue"
+    },
+    "manjaro": {
+        "lines": [
+            " ,___,,_, ",
+            " | ,_|| | ",
+            " | |  | | ",
+            " |_|__|_| ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "green",
+        "text_color": "green"
+    },
+    "nixos": {
+        "lines": [
+            "          ",
+            "   ==\\\\   ",
+            "  // //  ",
+            "   \\\\==   ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "black",
+        "text_color": "cyan"
+    },
+    "opensuse-leap": {
+        "lines": [
+            "    ,___  ",
+            "   _| ()\\ ",
+            " /    --' ",
+            " \\ __^/   ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "green",
+        "text_color": "green"
+    },
+    "opensuse-tumbleweed": {
+        "lines": [
+            "  _    _  ",
+            " / \\  / \\ ",
+            " |  )(  | ",
+            " \\_/  \\_/ ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "blue",
+        "text_color": "blue"
+    },
+    "ubuntu": {
+        "lines": [
+            "    __    ",
+            " () -- () ",
+            " | |  | | ",
+            "  \\ -- /  ",
+            "    ''    "
+        ],
+        "fore_color": "black",
+        "bg_color": "yellow",
+        "text_color": "yellow"
+    },
+    "slackware": {
+        "lines": [
+            "          ",
+            "   ,-=|   ",
+            "   '==,   ",
+            "   |=-'   ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "blue",
+        "text_color": "blue"
+    },
+    "arco": {
+        "lines": [
+            "          ",
+            "    /\\    ",
+            "   //\\\\   ",
+            "  // -=\\  ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "blue",
+        "text_color": "blue"
+    },
+    "": {
+        "lines": [
+            "          ",
+            "          ",
+            "          ",
+            "          ",
+            "          "
+        ],
+        "fore_color": "black",
+        "bg_color": "white",
+        "text_color": "white"
+    }
+}
